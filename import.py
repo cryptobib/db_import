cryptobib/db_import

commit a25038abbded24c399566b20f04ef7adadb81261

better support for bibtex keys with authors_subs_re

import.py
@@ -34,10 +34,14 @@ from config import *
 logging_colorer.init()
 logging.basicConfig(level=logging.DEBUG)
 
-# Fix for authors names (used by `author_subs_replace`)
+# Fix for authors names (used by `get_author_name_and_for_key`)
 
 # A dictionary of regex to fix authors names
-# If the author matches the key, it is replace (using re.sub) by the value
+# If the author matches the key, 
+#   if the value is a string, it is replaced by it
+#   otherwise, the value is a pair of two strings
+#      the author is then replaced by the first part of the pair
+#      and the second part of the pair is the last name used for the BibTeX key
 author_subs_re = {
     ur"Ruba Abu-Salma": ur"Ruba {Abu-Salma}",
     ur"Carlos Aguilar Melchor": ur"Carlos {Aguilar Melchor}",
@@ -54,7 +58,7 @@ author_subs_re = {
     ur"Dana Dachman-Soled": ur"Dana {Dachman-Soled}",
     ur"Christophe [dD]e Canni[èe]re": ur"Christophe {De Canni{\`e}re}",
     ur"Sabrina [dD]e Capitani [dD]i Vimercati": ur"Sabrina {De Capitani di Vimercati}",
-    ur"Angelo [dD]e Caro": ur"Angelo {De Caro}",
+    ur"Angelo [dD]e Caro": (ur"Angelo {De Caro}", ur"DeCaro"),
     ur"Emiliano [Dd]e Cristofaro": ur"Emiliano {De Cristofaro}",
     ur"Guerric Meurice [dD]e Dormale": ur"Guerric Meurice {de Dormale}",
     ur"Luca [dD]e Feo": ur"Luca {De Feo}",
@@ -138,23 +142,42 @@ author_subs_re_compiled = {
     re.compile(r): s
     for r, s in author_subs_re.iteritems()
 }
-author_subs_re_all = re.compile("|".join(author_subs_re.keys()))
-print(u"|".join(author_subs_re.keys()))
+author_subs_re_all = re.compile("^" + "|".join(author_subs_re.keys()) + "$")
 
 
-def author_subs_replace(author):
-    """ fix the author name using author_subs_re"""
+def get_author_name_for_key(author):
+    """ return the author last name for key """
+    # TODO: this is not always OK, we should use pybtex.Person...
+    last_name = author.split(" ")[-1]
+    if last_name == "Jr." and len(author) > 1:
+        last_name = "".join(author.split(" ")[-2:])
+    # remove "." from the name if any as it is not allowed by pybtex in keys
+    return last_name.replace(".", "")
+
+
+def get_author_name_and_for_key(author):
+    """ 
+    Return a pair (author, last_name_bibtex) where 
+    - name is the cleaned author name using author_subs_re
+    - name_for_key is the last name to be used to generate BibTeX keys
+      it is either computed using author_subs_re or generated by get_author_name_for_key
+    """
 
     # For efficiency, we first use author_subs_re_all to find the places to
     # replace, but then manually sub
-    def sub(text):
-        print("---" + text)
+    def get_match(text):
         for r, s in author_subs_re_compiled.iteritems():
             if r.match(text):
-                return r.sub(s, text)
+                return s
 
-    res = author_subs_re_all.sub(lambda match: sub(match.group(0)), author)
-    return res
+    if author_subs_re_all.match(author):
+        s = get_match(author)
+        if isinstance(s, basestring):
+            return (s, get_author_name_for_key(s))
+        else:
+            return s
+    
+    return (author, get_author_name_for_key(author))
 
 
 # Remove accents
@@ -254,29 +277,22 @@ def clean_author(author):
         return author
 
 
-def get_author_name_for_key(author):
-    """ return the author last name for key """
-    # TODO: this is not always OK, we should use pybtex.Person...
-    last_name = author.split(" ")[-1]
-    if last_name == "Jr." and len(author) > 1:
-        last_name = "".join(author.split(" ")[-2:])
-    # remove "." from the name if any as it is not allowed by pybtex in keys
-    return last_name.replace(".", "")
-
-
-def authors_to_key(authors, confkey, short_year):
+def authors_to_key(authors_last_names, confkey, short_year):
     """ return the author bibtext key part """
-    if len(authors) <= 0:
+    if len(authors_last_names) <= 0:
         logging.error("Entry with no author => replaced by ???")
         return confkey + ":" + "???" + str(short_year)
-    elif len(authors) == 1:
-        return confkey + ":" + strip_accents(get_author_name_for_key(authors[0])) + str(short_year)
-    elif len(authors) <= 3:
-        return confkey + ":" + "".join((get_author_name_for_key(strip_accents(a))[:3] for a in authors)) + str(short_year)
+    elif len(authors_last_names) == 1:
+        # the key contains the last name
+        return confkey + ":" + strip_accents(authors_last_names[0]) + str(short_year)
+    elif len(authors_last_names) <= 3:
+        # the key contains the first three letters of each last name
+        return confkey + ":" + "".join(strip_accents(a)[:3] for a in authors_last_names) + str(short_year)
     else:
-        if len(authors) >= 6:
-            authors = authors[:6]
-        return confkey + ":" + "".join((get_author_name_for_key(strip_accents(a))[0] for a in authors)) + str(short_year)
+        # the key contains the first letter of the first siz authors
+        if len(authors_last_names) >= 6:
+            authors = authors_last_names[:6]
+        return confkey + ":" + "".join(strip_accents(a)[0] for a in authors_last_names) + str(short_year)
 
 
 pattern_non_alphanum = re.compile(r'(\W+)')
@@ -339,11 +355,11 @@ def xml_to_entry(xml, confkey, entry_type, fields, short_year):
         return None, None
 
     entry = {}
-    authors = []
+    authors = [] # list of pairs (full author name, last name for BibTeX key)
     pages_error = None
     for e in elt:
         if e.tag == "author":
-            authors.append(author_subs_replace(clean_author(unicode(e.text))))
+            authors.append(get_author_name_and_for_key(clean_author(unicode(e.text))))
         elif e.tag in fields:
             val = xml_get_value(e)  # e.text
             if e.tag == "pages":
@@ -371,8 +387,11 @@ def xml_to_entry(xml, confkey, entry_type, fields, short_year):
                 if "doi" not in entry:
                     entry["doi"] = html_to_bib_value(p.group(1))
 
-    entry["author"] = html_to_bib_value((u" and \n" + " " * 18).join(authors))
-    key = authors_to_key(authors, confkey, short_year)
+    authors_bibtex = [a[0] for a in authors]
+    entry["author"] = html_to_bib_value((u" and \n" + " " * 18).join(authors_bibtex))
+
+    authors_last_names = [a[1] for a in authors]
+    key = authors_to_key(authors_last_names, confkey, short_year)
 
     if pages_error is not None:
         logging.error("Entry \"{}\": error in pages (\"{}\")".format(key, pages_error))

commit cd3142b83808a3e3e9406637019fbae244522617

Fix an issue with authors names when importing new ePrint entries.
The generated bib file from ePrint used first names instead of last names.
This regression was introduced in commit a25038abbded24c399566b20f04ef7adadb81261.

import.py
@@ -803,7 +803,9 @@ def run(confkey, year, dis, overwrite=False):
             entry["howpublished"] = '"Cryptology ePrint Archive, Report {}/{}"'.format(entry["year"], eprint_id)
             entry["note"] = '"\url{{https://eprint.iacr.org/{}/{}}}"'.format(entry["year"], eprint_id)
             entry["author"] = html_to_bib_value((u" and \n" + " " * 18).join(authors))
-            key = authors_to_key(authors, confkey, short_year)
+
+            filtered_authors = filter(lambda item: len(item)>0, [get_author_name_and_for_key(auth)[1] for auth in authors])
+            key = authors_to_key(filtered_authors, confkey, short_year)
 
             if key in entries:
                 multiple_entries[key] = 1